ac849ddfae967ebc8e371d967b2c3f65fb7ec795
Jerred Shepherd <ShepherdJerred@gmail.com>
2018-04-04 20:40:57 -0500

    Add libraries to build, add qotd.txt file, finish project

--- main.cpp
+++ main.cpp
@@ -11,20 +11,8 @@ const char *port = "17";
 using std::cout;
 using std::endl;
 using std::ifstream;
-#pragma comment (lib,"ws2_32.lib")
-
-void printIp() {
-    char myIPAddress[16];
-    char broadcastAddress[16];
-
-    int status = getIPAddressInfo(myIPAddress, broadcastAddress);
-    if (status >= 0) {
-        cout << timestamp() << " - " << "Started qotd service on " << myIPAddress << ":" << port << endl;
-    } else {
-        cout << "Error getting IP address" << endl;
-        exit(1);
-    }
-}
+using std::string;
+#pragma comment (lib, "ws2_32.lib")
 
 void main() {
     WORD wVersionRequired = 0x0101;
@@ -35,7 +23,16 @@ void main() {
         return;
     }
 
-    printIp();
+    char myIPAddress[16];
+    char broadcastAddress[16];
+
+    int status = getIPAddressInfo(myIPAddress, broadcastAddress);
+    if (status >= 0) {
+        cout << timestamp() << " - " << "Started qotd service on " << myIPAddress << ":" << port << endl;
+    } else {
+        cout << "Error getting IP address" << endl;
+        exit(1);
+    }
 
     SOCKET s;
     s = passivesock(port, "UDP");
@@ -43,23 +40,42 @@ void main() {
     char buffer[MAX_RECV_BUF];
     char remoteHost[MAX_HOST_SIZE];
     char remotePort[MAX_PORT];
-    int numBytesReceived = UDP_recv(s, buffer, MAX_RECV_BUF, remoteHost, remotePort);
 
-    if (buffer == "sendQOTD") {
+    while (true) {
+        int numBytesReceived = UDP_recv(s, buffer, MAX_RECV_BUF, remoteHost, remotePort);
 
-    } else if (buffer == "shutdown") {
+        cout << timestamp() << " - " << "Client connected with " << remoteHost << ":" << remotePort << endl;
+        cout << timestamp() << " - " << "Command received: " << buffer << endl;
 
-    }
+        if (_stricmp("SENDQOTD", buffer) == 0) {
+            cout << timestamp() << " - " << "Sending QOTD" << endl;
 
-    ifstream fin("qotd.txt");
-    if (fin.is_open()) {
-        fin.clear();
-        char textLine[MAX_SIZE];
-        fin.getline(textLine, MAX_SIZE - 2);
-        while (!fin.eof()) {
-            strcat_s(textLine, MAX_SIZE, "\n");
-            int numberCharactersSent = UDP_send(s, textLine, strlen(textLine + 1), remoteHost, remotePort);
-            fin.getline(textLine, MAX_SIZE - 2);
+            ifstream fin("qotd.txt");
+            if (fin.is_open()) {
+                fin.clear();
+                char textLine[MAX_SIZE];
+                fin.getline(textLine, MAX_SIZE - 2);
+                while (!fin.eof()) {
+                    strcat_s(textLine, MAX_SIZE, "\n");
+                    int numberCharactersSent = UDP_send(s, textLine, strlen(textLine) + 1, remoteHost, remotePort);
+                    fin.getline(textLine, MAX_SIZE - 2);
+                }
+            }
+        } else if (_stricmp("SHUTDOWN", buffer) == 0) {
+            cout << timestamp() << " - " << "Shutting down" << endl;
+
+            if (strcmp(myIPAddress, remoteHost) == 0) {
+                char shutdownMessage[] = "Service: qotd - will be shutdown";
+                UDP_send(s, shutdownMessage, strlen(shutdownMessage) + 1, remoteHost, remotePort);
+                closesocket(s);
+                break;
+            } else {
+                char shutdownMessage[] = "SHUTDOWN request denied";
+                UDP_send(s, shutdownMessage, strlen(shutdownMessage) + 1, remoteHost, remotePort);
+            }
+        } else {
+            cout << timestamp() << " - " << "Invalid command" << endl;
         }
+
     }
 }
